5c9ddcc391a6c137fa11f439aee83218fe19175c
pilpod <67429595+pilpod@users.noreply.github.com>
2020-12-29 18:14:30 +0100

    Style page welcome finished

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,3 @@
-<div>
+<div class="row mt-3">
     <h1>Hello World...</h1>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts/layout')
 
 @section('content')
-    
-    @livewire('hello-world')
+
+    <img id="imgPortada" src="{{ asset('img/portada.jpg') }}">
+
+    <div class="container d-flex justify-content-center mt-3">
+        <a class="btn btn-primary" href="#">Go!</a>
+    </div>
 
 @endsection
